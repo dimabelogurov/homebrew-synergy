f26b7ec7315bff8cc412febeebab5605fa7440eb
Volodymyr Marchenko <Volodymyr_Marchenko1@epam.com>
2016-11-08 11:45:27 +0200

    Update version to v1.8.2-stable-36cd521

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -1,8 +1,8 @@
 cask 'synergy' do
-  version '1.7.6-rc3-51705f7'
-  sha256 '187724f3bbb1412549618b3135e921c708539618784c946dcabfc4edee45b8ba'
+  version 'v1.8.2-stable-36cd521'
+  sha256 '07851d85a7cbb5875c8be7111880d3ab5213b0ef48c63ab1b4dfef8defdd6bcc'
 
-  url 'http://synergy-project.org/files/nightly/synergy-v1.7.6-rc3-51705f7-MacOSX1011-x86_64.dmg'
+  url 'http://repo.uncle.com.hk/vault/Projects/synergy/synergy-v1.8.2-stable-36cd521-MacOSX1011-x86_64.dmg'
   name 'Synergy'
   homepage 'http://synergy-project.org/'
   license :gpl
